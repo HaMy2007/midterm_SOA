512dfda4da971100da806428f92771b2ce92b65a
Nguyen Ha My <52200066@student.tdtu.edu.vn>
2025-03-11 15:22:08 +0700

    Fix .gitignore

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const db = require('./src/config/db');
+const app = express();
+const PORT = process.env.PORT || 3000; 
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
