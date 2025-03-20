1e0bd4ce035ed3df7bb5d7043871a2eb2d188cc8
Nhanga007 <nhanga007@gmail.com>
2025-03-20 14:54:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,49 @@
 const express = require("express");
-const db = require('./src/config/db');
 const app = express();
 const PORT = process.env.PORT || 3000; 
 
 require('./src/config/db');
 app.use(express.json());
-const { createOrder, addItem, getOrderDetails, cancelItem, confirmItem, getPendingOrders} = require("./src/util/function");
 
-// Đăng ký route API: Tạo đơn hàng mới
+const { 
+  createOrder, 
+  addItem, 
+  getOrderDetails, 
+  cancelItem, 
+  confirmItem, 
+  getPendingOrders,
+  completeOrder,
+  getRevenue,
+  getOrderHistory
+} = require("./src/util/function");
+
+// API 1: Tạo đơn hàng
 app.post("/api/orders/create", createOrder);
 
-// Đăng ký route API: Thêm món vào đơn hàng
+// API 2: Thêm món
 app.post("/api/orders/add-item", addItem);
 
-// Đăng ký route API: Xem chi tiết đơn hàng
+// API 3: Xem chi tiết đơn hàng
 app.get("/api/orders/:order_id", getOrderDetails);
 
-// Đăng ký route API: Huỷ món đã đặt
+// API 4: Huỷ món
 app.delete("/api/orders/:order_id/item/:item_id", cancelItem);
 
-// Route API 5
-app.post("/api/kitchen/confirm-item", confirmItem); 
+// API 5: Xác nhận món đã hoàn thành
+app.post("/api/kitchen/confirm-item", confirmItem);
 
-// Route API 6
+// API 6: Xem danh sách đơn hàng đang chờ chế biến
 app.get("/api/kitchen/orders", getPendingOrders);
 
+// API 7: Hoàn tất đơn hàng
+app.post("/api/orders/complete", completeOrder);
+
+// API 8: Xem tổng hợp doanh thu
+app.get("/api/manager/revenue", getRevenue);
+
+// API 9: Xem lịch sử hóa đơn
+app.get("/api/manager/orders/history", getOrderHistory);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
